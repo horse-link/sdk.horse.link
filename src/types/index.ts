5fabfbf2f719b5c9f2dbfb8a2aa5bea2c3291082
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-06-27 18:44:36 +1000

    feat: add more types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+import * as config from "./config";
+import * as meets from "./meets";
+import * as tokens from "./tokens";
+import * as vaults from "./vaults";
+
+const types = {
+  config,
+  meets,
+  tokens,
+  vaults
+};
+
+export default types;
