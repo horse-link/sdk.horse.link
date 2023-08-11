b685b40d363e321217b404b4c20627b7219ec310
phi1 <80179235+Pacman1979@users.noreply.github.com>
2023-08-11 15:58:05 +1000

    Update config.ts
    Added 9 x explicit return types

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,49 +2,49 @@ import { Config, MarketInfo } from "../types/config";
 import { Token } from "../types/tokens";
 import { VaultInfo } from "../types/vaults";
 
-export const getTokenFromSymbol = (symbol: string, config?: Config) =>
+export const getTokenFromSymbol = (symbol: string, config?: Config): Token | undefined =>
   config?.tokens.find(
     token => token.symbol.toLowerCase() === symbol.toLowerCase()
   );
 
-export const isUsdt = (address: string, config?: Config) =>
+export const isUsdt = (address: string, config?: Config): boolean =>
   getTokenFromSymbol("USDT", config)?.address.toLowerCase() ===
   address.toLowerCase();
 
 export const getVaultNameFromMarket = (
   marketAddress: string,
   config?: Config
-) =>
+): string | undefined =>
   config?.vaults.find(
     vault => vault.marketAddress.toLowerCase() === marketAddress.toLowerCase()
   )?.name;
 
-export const getVault = (vaultAddress?: string, config?: Config) =>
+export const getVault = (vaultAddress?: string, config?: Config): VaultInfo | undefined =>
   config?.vaults.find(
     vault => vault.address.toLowerCase() === vaultAddress?.toLowerCase()
   );
 
-export const getVaultFromMarket = (market?: MarketInfo, config?: Config) =>
+export const getVaultFromMarket = (market?: MarketInfo, config?: Config): VaultInfo | undefined =>
   config?.vaults.find(
     vault => vault.address.toLowerCase() === market?.vaultAddress.toLowerCase()
   );
 
-export const getVaultFromAssetAddress = (address: string, config?: Config) =>
+export const getVaultFromAssetAddress = (address: string, config?: Config): VaultInfo | undefined =>
   config?.vaults.find(
     vault => vault.asset.address.toLowerCase() === address.toLowerCase()
   );
 
-export const getVaultFromToken = (token: Token, config?: Config) =>
+export const getVaultFromToken = (token: Token, config?: Config): VaultInfo | undefined =>
   config?.vaults.find(
     v => v.asset.address.toLowerCase() === token.address.toLowerCase()
   );
 
-export const getMarketFromVault = (vault: VaultInfo, config?: Config) =>
+export const getMarketFromVault = (vault: VaultInfo, config?: Config): MarketInfo | undefined =>
   config?.markets.find(
     m => m.vaultAddress.toLowerCase() === vault.address.toLowerCase()
   );
 
-export const getMarketFromToken = (token: Token, config?: Config) => {
+export const getMarketFromToken = (token: Token, config?: Config): MarketInfo | undefined => {
   const vault = getVaultFromToken(token, config);
   if (!vault) return;
 
