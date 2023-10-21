6f7d6977c4fb3ab4815f2ab59b27eb6567c2a6ba
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-10-21 17:38:27 +1000

    chore: update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,19 @@
-import * as config from "./config";
 import * as bets from "./bets";
+import * as config from "./config";
 import * as meets from "./meets";
 import * as race from "./race";
+import * as runner from "./runner";
 import * as stats from "./stats";
 import * as tokens from "./tokens";
 import * as vaults from "./vaults";
 
 const types = {
-  config,
   bets,
+  config,
   meets,
-  stats,
   race,
+  runner,
+  stats,
   tokens,
   vaults
 };
