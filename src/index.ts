8cbda86dc9e223dd579339b762b4ae5969bb87fa
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-11-30 09:18:41 +1000

    chore: fixes to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-export * from "./constants/index";
-export * from "./types/index";
-export * from "./utils/formatting";
-export * from "./utils/general";
+// export * from "./constants/index";
+// export * from "./types/index";
+// export * from "./utils/formatting";
+// export * from "./utils/general";
+
+import constants from "./constants";
+import * as types from "./types";
+import * as formatting from "./utils/formatting";
+import * as general from "./utils/general";
+
+const SDK = {
+  constants,
+  types,
+  formatting,
+  general
+};
+
+export default SDK;
