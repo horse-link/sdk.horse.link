cd151242391ce71bb795c9670f44828ecede0a0e
Barry Earsman <barry.earsman@dltx.io>
2023-12-17 14:59:13 +1000

    fix: tests

--- test/formatting.spec.ts
+++ test/formatting.spec.ts
@@ -1,5 +1,5 @@
 // import dayjs from "dayjs";
-import utils from "../src/utils";
+import * as formatting from "../src/utils/formatting"
 
 test.concurrent.each([
   ["0", "0.0000"],
@@ -13,7 +13,7 @@ test.concurrent.each([
   ["3456.3456", "3456.3456"],
   ["1.00000", "1.0000"]
 ])("should have trailing zero", async (input: string, expected: string): Promise<void> => {
-  const result = utils.formatting.formatToFourDecimals(input);
+  const result = formatting.formatToFourDecimals(input);
   expect(result).toBe(expected);
 });
 
@@ -24,7 +24,7 @@ test.concurrent.each([
   ["0.00056", "0.0006"],
   ["123456789.123456789", "123456789.1235"]
 ])("round to four decimal", async (input: string, expected: string): Promise<void> => {
-  const result = utils.formatting.formatToFourDecimals(input);
+  const result = formatting.formatToFourDecimals(input);
   expect(result).toBe(expected);
 });
 
@@ -38,7 +38,7 @@ test.concurrent.each([
   "0.000099",
   "0.000005132156"
 ])("less than 0.0001", async (input: string): Promise<void> => {
-  const result = utils.formatting.formatToFourDecimals(input);
+  const result = formatting.formatToFourDecimals(input);
   expect(result).toBe("<0.0001");
 });
 
@@ -49,20 +49,20 @@ test.concurrent.each([
   ["100.1045", "100.10"],
   ["1000.2895", "1000.29"]
 ])("round to two decimals", async (input: string, expected: string): Promise<void> => {
-  const result = utils.formatting.formatToTwoDecimals(input);
+  const result = formatting.formatToTwoDecimals(input);
   expect(result).toBe(expected);
 });
 
 describe("address tests", () => {
   it("should shorten address", () => {
-    const actual = utils.formatting.shortenAddress(
+    const actual = formatting.shortenAddress(
       "0x00000000219ab540356cBB839Cbe05303d7705Fa"
     );
     expect(actual).toEqual("0x000...705Fa");
   });
 
   it("should shorten hash", () => {
-    const actual = utils.formatting.shortenHash(
+    const actual = formatting.shortenHash(
       "0x28d9cb5e5a5c4c5d27cf9d8b91ca9b34c31452d105a7201ecd327876f816a592"
     );
     expect(actual).toEqual("0x28d9cb5e5a5c4...d327876f816a592");
