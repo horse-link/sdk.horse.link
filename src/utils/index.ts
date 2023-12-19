13da9fa6a2be9081fea177634e451987fddaca68
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-12-19 10:16:55 +1000

    feat: fix tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,3 @@ export * as errors from "../constants/errors";
 export * as formatting from "./formatting";
 export * as general from "./general";
 export * as scrub from "./scrub";
-
--- test/formatting.spec.ts
+++ test/formatting.spec.ts
@@ -1,5 +1,5 @@
 // import dayjs from "dayjs";
-import utils from "../src/utils";
+import { formatting } from "../src/utils";
 
 test.concurrent.each([
   ["0", "0.0000"],
@@ -12,10 +12,13 @@ test.concurrent.each([
   ["234.234", "234.2340"],
   ["3456.3456", "3456.3456"],
   ["1.00000", "1.0000"]
-])("should have trailing zero", async (input: string, expected: string): Promise<void> => {
-  const result = utils.formatting.formatToFourDecimals(input);
-  expect(result).toBe(expected);
-});
+])(
+  "should have trailing zero",
+  async (input: string, expected: string): Promise<void> => {
+    const result = formatting.formatToFourDecimals(input);
+    expect(result).toBe(expected);
+  }
+);
 
 test.concurrent.each([
   ["0.0000", "0.0000"],
@@ -23,10 +26,13 @@ test.concurrent.each([
   ["0.00055", "0.0006"],
   ["0.00056", "0.0006"],
   ["123456789.123456789", "123456789.1235"]
-])("round to four decimal", async (input: string, expected: string): Promise<void> => {
-  const result = utils.formatting.formatToFourDecimals(input);
-  expect(result).toBe(expected);
-});
+])(
+  "round to four decimal",
+  async (input: string, expected: string): Promise<void> => {
+    const result = formatting.formatToFourDecimals(input);
+    expect(result).toBe(expected);
+  }
+);
 
 test.concurrent.each([
   "0.00001",
@@ -38,7 +44,7 @@ test.concurrent.each([
   "0.000099",
   "0.000005132156"
 ])("less than 0.0001", async (input: string): Promise<void> => {
-  const result = utils.formatting.formatToFourDecimals(input);
+  const result = formatting.formatToFourDecimals(input);
   expect(result).toBe("<0.0001");
 });
 
@@ -48,21 +54,24 @@ test.concurrent.each([
   ["20.01", "20.01"],
   ["100.1045", "100.10"],
   ["1000.2895", "1000.29"]
-])("round to two decimals", async (input: string, expected: string): Promise<void> => {
-  const result = utils.formatting.formatToTwoDecimals(input);
-  expect(result).toBe(expected);
-});
+])(
+  "round to two decimals",
+  async (input: string, expected: string): Promise<void> => {
+    const result = formatting.formatToTwoDecimals(input);
+    expect(result).toBe(expected);
+  }
+);
 
 describe("address tests", () => {
   it("should shorten address", () => {
-    const actual = utils.formatting.shortenAddress(
+    const actual = formatting.shortenAddress(
       "0x00000000219ab540356cBB839Cbe05303d7705Fa"
     );
     expect(actual).toEqual("0x000...705Fa");
   });
 
   it("should shorten hash", () => {
-    const actual = utils.formatting.shortenHash(
+    const actual = formatting.shortenHash(
       "0x28d9cb5e5a5c4c5d27cf9d8b91ca9b34c31452d105a7201ecd327876f816a592"
     );
     expect(actual).toEqual("0x28d9cb5e5a5c4...d327876f816a592");
